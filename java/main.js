076db1870d77f9594c6806f440991db66b758850
wattsjake <watts.jacob.samuel@gmail.com>
2021-11-11 21:24:24 -0700

    new website design!

---
+++ java/main.js
@@ -0,0 +1,9 @@
+
+function mobile_menu() {
+  var x = document.getElementById("mobile-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
