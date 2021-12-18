9ee0757f14673edc7af2b9f15606d04ab9fef40f
wattsjake <watts.jacob.samuel@gmail.com>
2021-12-18 13:38:00 -0700

    created temperature converter with javascript

--- java/main.js
+++ java/main.js
@@ -7,3 +7,9 @@ function mobile_menu() {
     x.style.display = "block";
   }
 }
+
+/* When the input field receives input, convert the value from fahrenheit to celsius */
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  document.getElementById("outputCelsius").innerHTML = (valNum-32) / 1.8;
+}
